67730d579ddc2b4f96c962a61cdffb121d837cc7
Prashant R <smplsoln@gmail.com>
2021-09-06 11:53:58 +0000

    Add age, hundreds, area calcs js js

---
+++ age.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, birth, curYear) {
+  return `${name} is ${curYear - birth} years old.`;
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
